75257dd4c4544f97e83a0a9804c88ecc601d41e4
surya <suryaravichandran5555@gmail.com>
2025-02-15 16:00:22 +0530

    Completed Thiran_2k24

--- script.js
+++ script.js
@@ -33,3 +33,16 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+};
+
+scrollTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
